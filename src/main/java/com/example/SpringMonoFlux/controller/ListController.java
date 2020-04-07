249bc279dde250d65792614cded6d5eea47ad74d
BrajaTannady <brajatannady11@gmail.com>
2020-04-07 11:00:18 +0700

    Consul and Vault

--- src/main/java/com/example/SpringMonoFlux/controller/ListController.java
+++ src/main/java/com/example/SpringMonoFlux/controller/ListController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/todo")
 public class ListController {
   @Autowired
   ListService service;
